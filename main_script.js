186484d869683e4241eb682fae4277582d839255
itsCoolguy <cadanpil@gmail.com>
2023-03-12 18:14:48 +0000

    Unread notification changes

--- main_script.js
+++ main_script.js
@@ -1,14 +1,14 @@
 const notificationAmountText = document.querySelector('#notification-number');
-const unreadSymbols = document.querySelectorAll('.new-notification-icon');
+const unreadNotifications = document.querySelectorAll('.new-notification');
 const markReadButton = document.querySelector('#mark-all-read-button');
 let unreadShown = true;
 
 function markReadClicked() {
     if (unreadShown === true) {
         unreadShown = false;
-        unreadSymbols.forEach(symbol => {
-            symbol.style.display = 'none';
-        });
+        unreadNotifications.forEach(element => {
+            element.classList.remove('new-notification');
+        })
         notificationAmountText.innerHTML = '0';
         markReadButton.setAttribute('disabled', true);
     }
